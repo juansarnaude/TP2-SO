f0609b8324f1c4d5a0a7903dd8e7ab2d7a6b757d
AndresCarro <86923323+AndresCarro@users.noreply.github.com>
2022-05-25 16:47:41 -0300

    syscall pal userland B) (incompleto)

---
+++ Userland/SampleCodeModule/syslib.asm
@@ -0,0 +1,23 @@
+GLOBAL sys_write
+GLOBAL sys_read
+GLOBAL sys_open_file
+GLOBAL sys_close_file
+
+section .text
+sys_write:
+    push rbp
+    mov rbp, rsp
+    mov rax, 1
+    int 0x80
+    mov rsp, rbp
+    pop rbp
+    ret
+
+sys_read:
+    push rbp
+    mov rbp, rsp
+    mov rax, 0
+    int 0x80
+    mov rsp,rbp
+    pop rbp
+    ret
