23b2e2a276f2dd524e4db14d7cbfbcb9b5d70283
Juan Segundo Arnaude <juansarnaude@gmail.com>
2022-05-28 15:26:55 -0300

    syscall exit execve

--- Kernel/syscallDispatcher.c
+++ Kernel/syscallDispatcher.c
@@ -1,36 +1,49 @@
 #include <syscallDispatcher.h>
+#include <scheduler.h>
 
-static uint64_t sys_read(unsigned int fd,char* output, uint64_t count);
-static void sys_write(unsigned fd,const char* buffer, uint64_t count);
-static int sys_exec(int (*function)(int argc, char const * argv[]), char const *argv[]);
-static void sys_exit();
+static uint64_t sys_read(unsigned int fd, char *output, uint64_t count);
+static void sys_write(unsigned fd, const char *buffer, uint64_t count);
+static void sys_exit(uint64_t errn);
+static void sys_execve(char *pathname, char *argv[], char *envp[]);
+static uint64_t sys_divideWindow(uint64_t rdi);
+static uint64_t sys_changeWindow(uint64_t rdi);
 
-uint64_t syscallDispatcher(uint64_t rdi, uint64_t rsi, uint64_t rdx, uint64_t rax){
-    //TODO: Aumentar la cantidad de registros que nos pasan a 6.
-    //TODO: Fijarse si las interrupciones son atendidas mientras esta atendiendo a una syscall.
-    switch(rax){
-        case 0:
-            return sys_read((unsigned int)rdi, (char*)rsi,rdx);
-            break;
-        case 1:
-            sys_write((unsigned int)rdi, (char*)rsi,rdx);
-            break;
-        case 11:
-            return sys_exec((int (*)(int, char const **))rdi, (char const **)rsi);
-            break;
-        case 60:
-            sys_exit();
+uint64_t syscallDispatcher(uint64_t rdi, uint64_t rsi, uint64_t rdx, uint64_t rax)
+{
+    // TODO: Aumentar la cantidad de registros que nos pasan a 6.
+    // TODO: Fijarse si las interrupciones son atendidas mientras esta atendiendo a una syscall.
+    switch (rax)
+    {
+    case 0:
+        return sys_read((unsigned int)rdi, (char *)rsi, rdx);
+        break;
+    case 1:
+        sys_write((unsigned int)rdi, (char *)rsi, rdx);
+        break;
+    case 2:
+        sys_exit((unsigned int)rdi);
+        break;
+    case 3:
+        sys_execve((char *)rdi, (char **)rsi, (char **)rdx);
+        break;
+    case 69:
+        return sys_divideWindow(rdi);
+        break;
+    case 70:
+        return sys_changeWindow(rdi);
+        break;
     }
     return 0;
 }
 
-static uint64_t sys_read(unsigned int fd,char* output, uint64_t count){
+static uint64_t sys_read(unsigned int fd, char *output, uint64_t count)
+{
     switch (fd)
     {
     case STDIN:
         return readBuffer(output, count);
         break;
-    
+
     default:
         return 0;
     }
@@ -39,27 +52,28 @@ static uint64_t sys_read(unsigned int fd,char* output, uint64_t count){
 #define STRING_SIZE 100
 static char string[STRING_SIZE] = {0};
 
-static void sys_write(unsigned fd,const char* buffer, uint64_t count){
+static void sys_write(unsigned fd, const char *buffer, uint64_t count)
+{
     uint64_t i, j;
-    for (i = 0, j=0; i < count;i++)
+    for (i = 0, j = 0; i < count; i++)
     {
-        if (j == STRING_SIZE){
+        if (j == STRING_SIZE)
+        {
             switch (fd)
             {
             case STDOUT:
                 ncPrint(string);
                 break;
-    
+
             case STDERR:
                 ncPrintFormat(string, 0x0C);
                 break;
             default:
                 return;
             }
-            j=0;
+            j = 0;
         }
         string[j++] = buffer[i];
-        
     }
     string[i] = '\0';
     switch (fd)
@@ -73,19 +87,29 @@ static void sys_write(unsigned fd,const char* buffer, uint64_t count){
         break;
     default:
         return;
-    } 
-}
-
-static int sys_exec(int (*function)(int argc, char const * argv[]), char const *argv[]){
-    int argc = 0;
-    while (argv[argc] != (void*)0)
-    {
-        argc++;
     }
-    
-    return executeTask(function, argc, argv);
 }
 
-static void sys_exit(){
-    exitTask();
+static void sys_exit(uint64_t errn)
+{
+    nextTask();
+}
+
+static void sys_execve(char *pathname, char *argv[], char *envp[])
+{
+    int i = 0;
+    for (; argv[i] != NULL; i++)
+    {
+    }
+    i++;
+    executeTask(*pathname, i, *argv);
+}
+
+static uint64_t sys_divideWindow(uint64_t rdi)
+{
+    return ncWindows(rdi);
+}
+static uint64_t sys_changeWindow(uint64_t rdi)
+{
+    return ncCurrentWindow(rdi);
 }
