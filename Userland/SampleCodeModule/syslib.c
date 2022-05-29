585f4e33eca139580ff5292e03d70beae1f4a617
AndresCarro <86923323+AndresCarro@users.noreply.github.com>
2022-05-29 17:07:40 -0300

    update scanf y new strcmp

--- Userland/SampleCodeModule/syslib.c
+++ Userland/SampleCodeModule/syslib.c
@@ -11,7 +11,16 @@ unsigned int strlen(const char *s){
 }
 
 unsigned int scanf(char *s,unsigned int size){
-    return sys_read(0,s,size);
+    int i=0;
+    while(i < size){
+        char c =getChar();
+        if('\n' == c){
+            return i;
+        }
+        putChar(c);
+        i++;
+    }
+    return i;
 }
 
 char getChar(){
@@ -29,4 +38,20 @@ void puts(const char* s){
         putChar(*s);
         s++;
     }
+}
+
+//https://code.woboq.org/userspace/glibc/string/strcmp.c.html
+int strcmp (const char *p1, const char *p2){
+  const unsigned char *s1 = (const unsigned char *) p1;
+  const unsigned char *s2 = (const unsigned char *) p2;
+  unsigned char c1, c2;
+  do
+    {
+      c1 = (unsigned char) *s1++;
+      c2 = (unsigned char) *s2++;
+      if (c1 == '\0')
+        return c1 - c2;
+    }
+  while (c1 == c2);
+  return c1 - c2;
 }
