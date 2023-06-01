d9ce1823bd1d35ab0ddde2db5586c3e6b8d46858
MatiasWod <mwodtke@itba.edu.ar>
2023-06-01 13:55:47 -0300

    Pipe starting code

--- Kernel/include/defs.h
+++ Kernel/include/defs.h
@@ -7,35 +7,34 @@
 
 #include <stdint.h>
 
-#define NULL ((void*)0)
+#define NULL ((void *)0)
 
 /* Flags para derechos de acceso de los segmentos */
-#define ACS_PRESENT     0x80            /* segmento presente en memoria */
-#define ACS_CSEG        0x18            /* segmento de codigo */
-#define ACS_DSEG        0x10            /* segmento de datos */
-#define ACS_READ        0x02            /* segmento de lectura */
-#define ACS_WRITE       0x02            /* segmento de escritura */
-#define ACS_IDT         ACS_DSEG
-#define ACS_INT_386 	0x0E		/* Interrupt GATE 32 bits */
-#define ACS_INT         ( ACS_PRESENT | ACS_INT_386 )
+#define ACS_PRESENT 0x80 /* segmento presente en memoria */
+#define ACS_CSEG 0x18    /* segmento de codigo */
+#define ACS_DSEG 0x10    /* segmento de datos */
+#define ACS_READ 0x02    /* segmento de lectura */
+#define ACS_WRITE 0x02   /* segmento de escritura */
+#define ACS_IDT ACS_DSEG
+#define ACS_INT_386 0x0E /* Interrupt GATE 32 bits */
+#define ACS_INT (ACS_PRESENT | ACS_INT_386)
 
-
-#define ACS_CODE        (ACS_PRESENT | ACS_CSEG | ACS_READ)
-#define ACS_DATA        (ACS_PRESENT | ACS_DSEG | ACS_WRITE)
-#define ACS_STACK       (ACS_PRESENT | ACS_DSEG | ACS_WRITE)
+#define ACS_CODE (ACS_PRESENT | ACS_CSEG | ACS_READ)
+#define ACS_DATA (ACS_PRESENT | ACS_DSEG | ACS_WRITE)
+#define ACS_STACK (ACS_PRESENT | ACS_DSEG | ACS_WRITE)
 
 /* Registros en memoria en las syscalls */
 #define REG_AMOUNT 15
 #define RIP_POS REG_AMOUNT
-#define RFLAGS_POS (REG_AMOUNT+2)
-#define RSP_POS (REG_AMOUNT+3)
+#define RFLAGS_POS (REG_AMOUNT + 2)
+#define RSP_POS (REG_AMOUNT + 3)
 
-#define PIPESIZE  512
-#define MAX_FDS   16
+#define PIPESIZE 512
+#define MAX_FDS 16
 
 typedef int pid_t;
 
-//Blocked Queue
+// Blocked Queue
 typedef struct blockednode
 {
     pid_t pid;
@@ -51,37 +50,22 @@ typedef struct
 
 typedef BlockedQueueCDT *BlockedQueueADT;
 
-//Semaphores
-typedef struct {
-    char * name;
-    uint64_t value;         //it wont be negative, process that try to wait when 0 will be stacked in blockedProcess
-    uint8_t locked;            //if its locked its 1 if not 0;
+// Semaphores
+typedef struct
+{
+    char *name;
+    uint64_t value; // it wont be negative, process that try to wait when 0 will be stacked in blockedProcess
+    uint8_t locked; // if its locked its 1 if not 0;
     BlockedQueueADT blockedProcesses;
 } semaphore;
- 
-typedef semaphore * sem_t;
 
-/* Pipe */
-typedef struct {
-    char data[PIPESIZE];
-    unsigned int read_offset;
-    unsigned int write_offset;
-    unsigned int open_for_read;
-    unsigned int open_for_write;
-    BlockedQueueADT blocked_in_read;
-    BlockedQueueADT blocked_in_write;
-} Pipe;
+typedef semaphore *sem_t;
 
-typedef struct pipenode {
-    Pipe * p;
-    struct pipenode * next;
-} PipeNode;
-
-typedef PipeNode * PipeList;
-
-typedef struct {
+//Que es esto?
+typedef struct
+{
     unsigned int mode;
-    Pipe * pipe;
+    Pipe *pipe;
 } fd_t;
 
 typedef unsigned int priority_t;
--- Kernel/ipc/pipe.c
+++ Kernel/ipc/pipe.c
@@ -1,1 +1,90 @@
+#include <defs.h>
+#include <memoryManager.h>
+#include <queue.h>
+#include <scheduler.h>
+#include <semaphore.h>
 
+typedef struct
+{
+    char data[PIPESIZE];
+    unsigned int openR;
+    unsigned int openW;
+    sem_t canRead;
+    unsigned int indexR;
+    unsigned int indexW;
+    BlockedQueueADT queueW;
+    BlockedQueueADT queueR;
+} Pipe;
+
+typedef struct pipeNode
+{
+    Pipe *pipe;
+    struct pipeNode *next;
+    struct pipeNode *previous;
+} pipeNode;
+
+typedef pipeNode *pipeList;
+
+pipeList pipesList = NULL;
+
+Pipe *pipeOpen()
+{
+    Pipe *newPipe = (Pipe *)memoryManagerAlloc(sizeof(Pipe));
+    newPipe->openR = 0;
+    newPipe->openW = 1;
+    newPipe->queueR = newQueue();
+    newPipe->queueW = newQueue();
+
+    pipeNode *newPipeNode = (pipeNode *)memoryManagerAlloc(sizeof(pipeNode));
+    newPipeNode->pipe = newPipe;
+    newPipeNode->next = pipesList;
+    newPipeNode->previous = NULL;
+    pipesList = newPipeNode;
+    return newPipe;
+}
+
+// TODO tengo que ver como liberar las queues
+int pipeClose(Pipe *pipe)
+{
+    pipeNode *current = pipesList;
+    while (current != NULL && current->pipe != pipe)
+    {
+        current = current->next;
+    }
+    if (current == NULL)
+        return 0;
+
+    freeQueue(pipe->queueR);
+    freeQueue(pipe->queueR);
+    if (current->next != NULL)
+        current->next->previous = current->previous;
+    if (current->previous != NULL)
+        current->previous->next = current->next;
+    sem_close(pipe->openR);
+    sem_close(pipe->openW);
+    memory_manager_free(current);
+    memory_manager_free(pipe);
+    return 1;
+}
+
+int pipeRead(Pipe *pipe, char *msg, int size)
+{
+    pid_t pid = getCurrentPid();
+    enqueuePid(pipe->queueR, pid);
+    int i = 0;
+    while (pipe->indexR < pipe->indexW && i < size)
+    {
+        msg[i] = pipe->data[pipe->indexR];
+        pipe->indexR++;
+        i++;
+    }
+    return msg;
+}
+
+int pipeWrite(Pipe *pipe, char *data, int size)
+{
+    for (int i = 0; i < size; i++, pipe->indexW++)
+    {
+        pipe->data[pipe->indexW] = data[i];
+    }
+}
